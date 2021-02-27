a6e32d7073af2a80f9eb1792f14fa35a74068bef
unknown <habib.tee2009@gmail.com>
2021-02-27 12:25:35 +0600

    Shop Page Completed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
+    // console.log(props);
     const { img, name, seller, price, stock } = props.product;
     return (
         <div className="product">
@@ -16,7 +17,13 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button className="main-button"> <FontAwesomeIcon icon={faShoppingCart} /> Add To Cart</button>
+                <button className="main-button"
+                    onClick={() =>{
+                        props.handelAddProduct(props.product)}
+                    }
+                >
+                    <FontAwesomeIcon icon={faShoppingCart} /> Add To Cart
+                </button>
             </div>
         </div>
     );
